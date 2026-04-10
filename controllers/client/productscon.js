e85a6fa24419c3711c30c20e79664ac60c7226f3
Cuongninhduc <ninhcuong248@gmail.com>
2026-04-11 00:35:03 +0700

    Capnhat

--- controllers/client/productscon.js
+++ controllers/client/productscon.js
@@ -1,5 +1,17 @@
-module.exports.index = (req,res) => {
+const Product = require("../../models/productmodel")
+
+module.exports.index = async (req,res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+    products.forEach(item => {
+        item.priceNew = (item.price*(100 - item.discountPercentage)/100).toFixed(0);
+    })
+    console.log(products)
+
     res.render("client/pages/products/index", {
-        pageTitle: "Danh sách"
+        pageTitle: "Danh sách",
+        products: products
     });
 }
