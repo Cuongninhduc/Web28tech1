e3880c1655370c8327dc0fcd756bed876d14f39e
Cuongninhduc <ninhcuong248@gmail.com>
2026-04-28 23:33:11 +0700

    Dung layout

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const database = require("./config/databasse.js")
 const app = express();
 const port = process.env.PORT;
 
+const systemConfig = require("./config/system");
+
+const routeAdmin = require("./routes/admin/index.route")
 const route = require("./routes/client/indexroutes.js")
 
 database.connect();
@@ -13,9 +16,15 @@ database.connect();
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+// App locals Variable
+app.locals.prefixAdmin = systemConfig.prefixAdmin
+
 // Nhúng file tĩnh
 app.use(express.static("public"));
+
+// Routes
 route(app)
+routeAdmin(app)
 // Chạy server
 app.listen(port, () => {
     console.log(`Server đang chạy tại trang này http://localhost:${port}`);
